37b8858bf8d82bfa059369990db99b0a8692797b
kwanwoo0122 <kwanwoo0122@naver.com>
2020-08-19 12:49:18 +0900

    feat: connect mongoose

---
+++ server.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+require('dotenv').config({path: 'variables.env'});
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true
+}, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to database successfully");
+  }
+})
