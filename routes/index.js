5ac4db2b8abc6fbcfbcaf5a6525d76b8434dac2e
kwanwoo0122 <kwanwoo0122@naver.com>
2020-08-19 12:49:41 +0900

    fix: change es6

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express';
+
+const router = Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
